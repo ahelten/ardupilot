d5bc371ad44393b43ff6b2a06df6ff9873d62d2e
Andy Helten <andy.helten@ieee.org>
2024-02-27 14:13:24 -0600

    Update version for new '-2' release, mainly trying to reduce message latencies

--- Rover/version.h
+++ Rover/version.h
@@ -6,7 +6,8 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduRover V4.4.0-1-gfr"
+// NOTE: The 'N' in 'Vx.y.z-N-gfr' needs to match the 'N' in '(FIRMWARE_VERSION_TYPE_DEV+N)' to set FW_TYPE
+#define THISFIRMWARE "ArduRover V4.4.0-2-gfr"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,4,0,FIRMWARE_VERSION_TYPE_OFFICIAL
@@ -14,6 +15,6 @@
 #define FW_MAJOR 4
 #define FW_MINOR 4
 #define FW_PATCH 0
-#define FW_TYPE FIRMWARE_VERSION_TYPE_OFFICIAL
+#define FW_TYPE (FIRMWARE_VERSION_TYPE_DEV+2)
 
 #include <AP_Common/AP_FWVersionDefine.h>
